bdf84e9a1f2a1d9e08bca3bda1ab2beeba1cfdc8
carlganz <carlganz@gmail.com>
2017-02-08 22:52:36 +0000

    use listings for code

--- R/CSUN.R
+++ R/CSUN.R
@@ -1,8 +1,13 @@
 #' CSUN Thesis Template
 #'
+#' Write your thesis for CSUN in R Markdown
+#'
 #' @export
 #' @author Carl Ganz
 #' @importFrom rmarkdown pdf_document
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#' @references href{https://www.sharelatex.com/templates/thesis/csun-thesis}
 #'
 
 csun_thesis <- function(...,
@@ -18,5 +23,22 @@ csun_thesis <- function(...,
     pandoc_args = pandoc_args
   )
   fmt$inherits <- "pdf_document"
+  fmt$knitr$knit_hooks$source <- function(x, options) {
+    if (options$prompt && length(x)) {
+      x <- gsub("\\n", paste0("\n", getOption("continue")), x)
+      x <- paste0(getOption("prompt"), x)
+    }
+    paste0(
+      c(
+        '\n\\begin{lstlisting}[language=',
+        options$engine,
+        "]",
+        x,
+        '\\end{lstlisting}',
+        ''
+      ),
+      collapse = '\n'
+    )
+  }
   fmt
 }
